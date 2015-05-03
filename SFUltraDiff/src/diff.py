42f049c60f837b18ff08120bc25f6869875fe30a
Eric dantarion Sheppard <dantarion@gmail.com>
2015-05-02 18:12:09 -0700

    Code for github ultradiff

--- SFUltraDiff/src/diff.py
+++ SFUltraDiff/src/diff.py
@@ -4,130 +4,12 @@ import sys
 import util
 import difflib
 from collections import OrderedDict
-HEADER = '''
-    <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dantarion's SFUltraDiff BETA</title>
+HEADER = '''---
+layout: default
+---
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-        <style type="text/css">
-        table.diff {{font-family:Consolas; border:medium;}}
-        .diff_header {{background-color:#e0e0e0}}
-        td.diff_header {{text-align:right}}
-        .diff_next {{background-color:#c0c0c0}}
-        .diff_add {{background-color:#aaffaa}}
-        .diff_chg {{background-color:#ffff77}}
-        .diff_sub {{background-color:#ffaaaa}}
-        .panel {{margin:10px}}
-        .panel .panel {{margin:5px}}
-        .anchor {{display: block; position: relative; top: -100px; visibility: hidden;}}
-    </style>
-  </head>
-  <body>
-  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Dantarion's SFUltraDiff BETA - {0}_{1}</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">{1}<b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="{0}_VANILLA_TO_SUPER.html">VANILLA_TO_SUPER</a></li>
-                <li><a href="{0}_SUPER_TO_AE.html">SUPER_TO_AE</a></li>
-                <li><a href="{0}_AE_TO_AE2012.html">AE_TO_AE2012</a></li>
-                <li><a href="{0}_AE2012_TO_ULTRA.html">AE2012_TO_ULTRA</a></li>
-                <li><a href="{0}_ULTRA_TO_ULTRA104.html"></a></li>
-              </ul>
-            </li>
-            <li class="divider"></li>
-            <li><a href="#Charges">Charges</a></li>
-            <li><a href="#Inputs">Inputs</a></li>
-            <li><a href="#Moves">Moves</a></li>
-            <li><a href="#Charges">CancelLists</a></li>
-            <li class="divider"></li>
-            <li><a href="#Floats">Floats</a></li>
-            <li><a href="#Scripts">Scripts</a></li>
-            <li><a href="#VFXScripts">VFXScripts</a></li>
-            <li><a href="#HitboxTable">HitboxTable</a></li>
-
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="http://twitter.com/dantarion">Contact @dantarion!</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-<div class= "container-fluid" style="padding-top:60px">
-    <div class='col-md-2'>
-            <div class="list-group">
-<a href="ADN_VANILLA_TO_SUPER.html" class="list-group-item">ADN</a>
-<a href="AGL_VANILLA_TO_SUPER.html" class="list-group-item">AGL</a>
-<a href="BLK_VANILLA_TO_SUPER.html" class="list-group-item">BLK</a>
-<a href="BLR_VANILLA_TO_SUPER.html" class="list-group-item">BLR</a>
-<a href="BOS_VANILLA_TO_SUPER.html" class="list-group-item">BOS</a>
-<a href="BSN_VANILLA_TO_SUPER.html" class="list-group-item">BSN</a>
-<a href="CDY_VANILLA_TO_SUPER.html" class="list-group-item">CDY</a>
-<a href="CHB_VANILLA_TO_SUPER.html" class="list-group-item">CHB</a>
-<a href="CMY_VANILLA_TO_SUPER.html" class="list-group-item">CMY</a>
-<a href="CNL_VANILLA_TO_SUPER.html" class="list-group-item">CNL</a>
-<a href="DAN_VANILLA_TO_SUPER.html" class="list-group-item">DAN</a>
-<a href="DDL_VANILLA_TO_SUPER.html" class="list-group-item">DDL</a>
-<a href="DJY_VANILLA_TO_SUPER.html" class="list-group-item">DJY</a>
-<a href="DSM_VANILLA_TO_SUPER.html" class="list-group-item">DSM</a>
-<a href="FLN_VANILLA_TO_SUPER.html" class="list-group-item">FLN</a>
-<a href="GEN_VANILLA_TO_SUPER.html" class="list-group-item">GEN</a>
-<a href="GKI_VANILLA_TO_SUPER.html" class="list-group-item">GKI</a>
-<a href="GKN_VANILLA_TO_SUPER.html" class="list-group-item">GKN</a>
-<a href="GUL_VANILLA_TO_SUPER.html" class="list-group-item">GUL</a>
-<a href="GUY_VANILLA_TO_SUPER.html" class="list-group-item">GUY</a>
-<a href="HKN_VANILLA_TO_SUPER.html" class="list-group-item">HKN</a>
-<a href="HND_VANILLA_TO_SUPER.html" class="list-group-item">HND</a>
-<a href="HWK_VANILLA_TO_SUPER.html" class="list-group-item">HWK</a>
-<a href="IBK_VANILLA_TO_SUPER.html" class="list-group-item">IBK</a>
-<a href="JHA_VANILLA_TO_SUPER.html" class="list-group-item">JHA</a>
-<a href="JRI_VANILLA_TO_SUPER.html" class="list-group-item">JRI</a>
-<a href="KEN_VANILLA_TO_SUPER.html" class="list-group-item">KEN</a>
-<a href="MKT_VANILLA_TO_SUPER.html" class="list-group-item">MKT</a>
-<a href="RIC_VANILLA_TO_SUPER.html" class="list-group-item">RIC</a>
-<a href="ROS_VANILLA_TO_SUPER.html" class="list-group-item">ROS</a>
-<a href="RYU_VANILLA_TO_SUPER.html" class="list-group-item">RYU</a>
-<a href="SGT_VANILLA_TO_SUPER.html" class="list-group-item">SGT</a>
-<a href="SKR_VANILLA_TO_SUPER.html" class="list-group-item">SKR</a>
-<a href="VEG_VANILLA_TO_SUPER.html" class="list-group-item">VEG</a>
-<a href="ZGF_VANILLA_TO_SUPER.html" class="list-group-item">ZGF</a>
-            </div>
-    </div>
-    <div class='col-md-10'>
-  '''
-FOOTER = '''</div></div><!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>'''
+'''
+FOOTER = ''''''
 def diffCollectionsHeader(col1,col2,log,level=0):
     removedSet = set(col1.keys()).difference(set(col2.keys()))
     addedSet = set(col2.keys()).difference(set(col1.keys()))
@@ -238,60 +120,81 @@ def dumpChar(char):
         
     
 def compareChar(char):
+    html = "<tr>"
     postfix = char+"\\"+char
-
-    VANILLA = "M:\\Xbox\\Ultra Xbox\\content\\04_ae2\\battle\\regulation\\sf4\\"
-    SUPER = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Super Street Fighter IV - Arcade Edition\\resource\\battle\\chara\\"
-    AE = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Super Street Fighter IV - Arcade Edition\\dlc\\03_character_free\\battle\\regulation\\latest\\"
-    AE2012 = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Super Street Fighter IV - Arcade Edition\\patch\\battle\\regulation\\latest_ae\\"
-    ULTRA = "M:\\Xbox\\Ultra Xbox\\content\\04_ae2\\battle\\regulation\\ae2\\"
-    ULTRA_TU1 = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\Super Street Fighter IV - Arcade Edition\\patch_ae2_tu1\\battle\\regulation\\ae2_109\\"
-    ULTRA_TU2 = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\Super Street Fighter IV - Arcade Edition\\patch_ae2_tu1b\\battle\\regulation\\ae2_109b\\"
-    ULTRA_TU3 = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\Super Street Fighter IV - Arcade Edition\\patch_ae2_tu2\\battle\\regulation\\ae2_110\\"
-    #versions = [VANILLA,SUPER,AE,AE2012,ULTRA,ULTRA_TU1,ULTRA_TU2,ULTRA_TU3]
-    bacs = {}
-    bcms = {}
-    #if char not in ["SGT","CHB"]:
-    #    return
-    #VANILLA,SUPER,AE,AE2012,
-    #"VANILLA","SUPER","AE","AE2012"
-    versions = [ULTRA_TU1,ULTRA_TU3]
-    names = ["ULTRA_TU1","ULTRA_TU3"]
+    BASE = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Super Street Fighter IV - Arcade Edition\\"
+    OUT = "D:\\sf4tools\\gh-pages\\"
+    if not os.path.exists(OUT+"characters\\"+char):
+        os.makedirs(OUT+"characters\\"+char)
+    paths = []
+    names = []
+    paths.append("resource\\battle\\chara")
+    names.append("Super")
+    paths.append("dlc\\03_character_free\\battle\\regulation\\latest")
+    names.append("AE")
+    paths.append("patch\\battle\\regulation\\latest_ae")
+    names.append("AE2012")
+    paths.append("patch\\battle\\regulation\\v101")
+    names.append("AE2012 v1.01")
+    paths.append("patch\\battle\\regulation\\v104")
+    names.append("AE2012 v1.04")
+    paths.append("dlc\\04_ae2\\battle\\regulation\\ae2")
+    names.append("Ultra v1.02")
+    paths.append("patch_ae2_tu1\\battle\\regulation\\ae2_109")
+    names.append("Ultra v1.03")
+    paths.append("patch_ae2_tu1b\\battle\\regulation\\ae2_109b")
+    names.append("Ultra v1.03b")
+    paths.append("patch_ae2_tu2\\battle\\regulation\\ae2_110")
+    names.append("Ultra v1.04")
+    paths.append("patch_ae2_tu3\\battle\\regulation\\ae2_111")
+    names.append("Ultra v1.05")
+    #paths.append("dlc\\04_ae2\\battle\\regulation\\sf4")
+    #names.append("Edition Select Vanilla Characters Release")
+    #paths.append("patch_ae2_tu1\\battle\\regulation\\sf4_109")
+    #names.append("Edition Select Vanilla Characters Title Update 1 (1.03)")
+    #paths.append("patch_ae2_tu2\\battle\\regulation\\sf4_110")
+    #names.append("Edition Select Vanilla Characters Title Update 2 (1.04)")
+    #paths.append("patch_ae2_tu2\\battle\\regulation\\concept")
+    #names.append("Omega Mode")
+    #paths.append("patch_ae2_tu3\\battle\\regulation\\concept_111")
+    #names.append("Omega Mode Title Update 1 (1.05)")
     
-    if os.path.isfile(ULTRA_TU2+postfix+".bac"):
-        return
     
-    for i,version in enumerate(versions):
+    bacs = []
+    bcms = []
+ 
+    namescopy = list(names)
+    for i,version in enumerate(list(paths)):
 
-        if not os.path.isfile(version+postfix+".bac"):
+        if not os.path.isfile(BASE+version+"\\"+postfix+".bac"):
+            
+            print char,"doesn't have",namescopy[i]
+            paths.remove(version)
+            names.remove(namescopy[i])
             continue
-        if os.path.isfile( "../json/"+char+"_"+names[i]+".bac.json"):
+        if os.path.isfile( "../json/"+char+"_"+namescopy[i]+".bac.json"):
             pass
-        print("\t"+names[i])
-        tmp = bac.BACFile(version+postfix+".bac")
-        log = open("../json/"+char+"_"+names[i]+".bac.json","w")
+        print("\t"+namescopy[i])
+        tmp = bac.BACFile(BASE+version+"\\"+postfix+".bac")
+        log = open("../json/"+char+"_"+namescopy[i]+".bac.json","w")
         log.write(tmp.toJSON())
-        bacs[i] = tmp
+        bacs.append(tmp)
         log.close()
         
-        tmp = bcm.BCMFile(version+postfix+".bcm")
-        log = open("../json/"+char+"_"+names[i]+".bcm.json","w")
+        tmp = bcm.BCMFile(BASE+version+"\\"+postfix+".bcm")
+        log = open("../json/"+char+"_"+namescopy[i]+".bcm.json","w")
         log.write(tmp.toJSON())
         #tmp.toFile("../out/"+char+"_"+names[i]+".bcm")
-        bcms[i] = tmp
+        bcms.append(tmp)
         log.close()
         
     
-    for i in range(0,len(versions)-1):
+    for i in range(0,len(paths)-1):
         
         name = names[i]+"_TO_"+names[i+1]
         
-        if not os.path.isfile(versions[i]+postfix+".bac"):
-            continue
-        if os.path.isfile( "../html/"+char+"_"+name+".html"):
-            pass
-        print("\tDoing ",name)
-        log = open("../html/"+char+"_"+name+".html","w")
+        print "\tDoing ",name
+        log = open(OUT+"characters\\"+char+"\\"+name+".html","w")
         log.write(HEADER.format(char,name))
         if len(bacs) < 2 or len(bcms) < 2:
             return;
@@ -349,9 +252,9 @@ def compareChar(char):
 
 
 import os
-#compareChar("RLN")
+#compareChar("RYU")
 #compareChar("SGT")
-for char in os.listdir("M:\\Xbox\\Ultra Xbox\\content\\04_ae2\\battle\\regulation\\ae2\\"):
+for char in os.listdir("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Super Street Fighter IV - Arcade Edition\\dlc\\04_ae2\\battle\\regulation\\ae2"):
    print(char)
    compareChar(char)
 
